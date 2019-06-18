c3291867d7ab147a9bc5e8a8722c7ecb61dadae6
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-06-17 21:31:14 -0400

    added beginning of jQuery functions

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+var yourCharacter;
+var enemies;
+var defender;
+
+var obiWan = {
+    hp: 120,
+    attack: 8
+};
+
+var luke = {
+    hp: 100,
+    attack: 5
+};
+
+var darthVader = {
+    hp: 150,
+    attack: 20,
+};
+
+var darthMaul = {
+    hp: 180, 
+    attack: 25
+};
+
+// Functions
+
+$("#attacker").on("click", function() {
+    $("#imageSection").appendTo("#attacker");
+
+});
+
+
+});
